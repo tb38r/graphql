39c4c6d300de9431a7c6c0340e9769473f0a8563
tb38r <tlawal86@hotmail.co.uk>
2022-11-14 17:34:11 +0000

    add and query

---
+++ basicdata.js
@@ -0,0 +1,21 @@
+let headerDiv = document.getElementsByClassName('graphdata')
+
+
+let basicDataQuery = `
+{ user (where:{login:{_eq:"tb38r"}}){
+    id
+    login
+    
+  }}
+`
+fetchQuery(userOptions(basicDataQuery))
+.then((data)=>{
+    Object.entries(data.data.user[0]).forEach(
+        ([key, value]) => {
+            let headerdata = document.createElement('div');
+            headerdata.innerHTML = `${key}:${value}`  
+            headerDiv[0].appendChild(headerdata)
+            
+         })
+
+})
